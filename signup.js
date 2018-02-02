7f42ffcb096de5140e7c88d4c6eabe9b54a8670c
mzwae <mrmonjaie@gmail.com>
2018-02-02 22:20:59 +0000

    added signing up

---
+++ signup.js
@@ -0,0 +1,39 @@
+function checkUser(user) {
+  if (user.value == '') {
+    O('info').innerHTML = '';
+    return;
+  }
+
+  var params = "user=" + user.value;
+  var request = new ajaxRequest();
+  request.open("POST", "checkuser.php", true);
+  request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  reuest.setRequestHeader("Content-length", params.length);
+  request.setRequestHeader("Connection", "close");
+
+  request.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200 && this.responseText != null) {
+      O('info').innerHTML = this.responseText;
+    }
+  }
+
+  request.send(params);
+}
+
+function ajaxRequest() {
+  try {
+    var request = new XMLHttpRequest();
+  } catch (e1) {
+    try {
+      request = new ActiveXObject("Msxml2.XMLHTTP");
+    } catch (e2) {
+      try {
+        request = new ActiveXObject("Microsoft.XMLHTTP");
+      } catch (e3) {
+        request = false;
+      }
+    }
+
+  }
+  return false;
+}
