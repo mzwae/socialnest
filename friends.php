be6373be1ddbe058c4d155893ecc9ae564085733
mzwae <mrmonjaie@gmail.com>
2018-02-09 18:47:54 +0000

    fixed bug in friends

--- friends.php
+++ friends.php
@@ -33,13 +33,21 @@ $num = $result->num_rows;
 
 for ($j=0; $j < $num; ++$j) {
     $row = $result->fetch_array(MYSQLI_ASSOC);
-    $following[$j] = $row['user'];
+    $followers[$j] = $row['friend'];
+}
+
+$result = queryMysql("SELECT * FROM friends WHERE friend='$view'");
+$num = $result->num_rows;
+
+for ($j=0; $j < $num; ++$j) {
+  $row = $result->fetch_array(MYSQLI_ASSOC);
+  $following[$j] = $row['user'];
 }
 
 $mutual = array_intersect($followers, $following);
 $followers = array_diff($followers, $mutual);
 $following = array_diff($following, $mutual);
-$friends = false;
+$friends = FALSE;
 
 if (sizeof($mutual)) {
     echo "<span class='subhead'>$name2 mutual friends</span><ul>";
